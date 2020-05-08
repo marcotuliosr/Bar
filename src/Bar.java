45a964f51528314e7d672b62721de49f6fd38221
marcotuliosr <marcotuliopessoal@gmail.com>
2020-05-08 10:55:35 -0300

    Leitura dos dados

--- src/Bar.java
+++ src/Bar.java
@@ -1,9 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Bar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		char sexo;
+		int qtdCervejas, qtdRefri, qtdEspetos;
+
+		System.out.print("Sexo: ");
+		sexo = sc.next().charAt(0);
+		System.out.print("Quantidade de cervejas: ");
+		qtdCervejas = sc.nextInt();
+		System.out.print("Quantidade de refrigerantes: ");
+		qtdRefri = sc.nextInt();
+		System.out.print("Quantidade de espetinhos: ");
+		qtdEspetos = sc.nextInt();
+
+		
 	}
 
 }
