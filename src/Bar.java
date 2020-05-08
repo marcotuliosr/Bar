fd7babad3cb0cf5a134e9cbd0cff74fdff8a93ff
marcotuliosr <marcotuliopessoal@gmail.com>
2020-05-08 11:04:44 -0300

    Relatório gerado

--- src/Bar.java
+++ src/Bar.java
@@ -10,6 +10,7 @@ public class Bar {
 
 		char sexo;
 		int qtdCervejas, qtdRefri, qtdEspetos;
+		double consumo, couvert, ingresso, total;
 
 		System.out.print("Sexo: ");
 		sexo = sc.next().charAt(0);
@@ -20,6 +21,37 @@ public class Bar {
 		System.out.print("Quantidade de espetinhos: ");
 		qtdEspetos = sc.nextInt();
 
+		
+		if (sexo == 'F') {
+			ingresso = 8.0;
+		}
+		else {
+			ingresso = 10.0;
+		}
+
+		consumo = qtdCervejas* 5.0 + qtdRefri * 3.0 + qtdEspetos * 7.0;
+
+		if (consumo > 30) {
+			couvert = 0.0;
+		}
+		else {
+			couvert = 4.0;
+		}
+
+		total = ingresso + consumo + couvert;
+
+		System.out.println();
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if (couvert == 0.0) {
+			System.out.println("Isento de Couvert");
+		}
+		else {
+			System.out.printf("Couvert = R$ %.2f%n", couvert);
+		}
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f%n", total);
 		
 	}
 
